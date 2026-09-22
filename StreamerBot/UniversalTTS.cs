e2915382fbcf36222ec3215c4434b797908fd03e
CHANOxDR <62312634+CHANOxDR@users.noreply.github.com>
2026-09-22 03:44:07 -0600

    Update UniversalTTS.cs
    Comentarios explicando  el uso de limite de caracteres y el aviso de quien manda el mensaje

--- StreamerBot/UniversalTTS.cs
+++ StreamerBot/UniversalTTS.cs
@@ -18,7 +18,8 @@ public class CPHInline
                 CPH.LogWarn("LoquendoBridge: no se recibió rawInput.");
                 return false;
             }
-
+            //Limite de caracteres del mensaje, si cambias el limite debes agregar esta linea a Arguments
+            //--max-length ElNumeroDeseado
             if (input.Length > 300)
                 input = input.Substring(0, 300);
 
@@ -27,7 +28,7 @@ public class CPHInline
             string voice = "Jorge";
 
             // Ajusta esta ruta a la ubicación de tu LoquendoBridge.exe.
-            string exePath = @"C:\LoquendoBridge\LoquendoBridge.exe";
+            string exePath = @"C:\Users\chano\Downloads\Programs\Loquendo Bridge v1.0\LoquendoBridge.exe";
 
             if (!File.Exists(exePath))
             {
@@ -35,13 +36,18 @@ public class CPHInline
                 return false;
             }
 
+            // Antes de cada mensaje se mencionara el usuario y "dice"  
+            // si no te gusta esta opcion puedes comentar las lineas
+            string user = args.ContainsKey("user") ? args["user"]?.ToString() : "usuario";
+            input = "@" + user + " dice: \"" + input + "\"";
+
             string base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(input));
 
             ProcessStartInfo psi = new ProcessStartInfo
             {
                 FileName = exePath,
                 // Si no usas VB-CABLE, cambia CABLE Input por default.
-                Arguments = "--voice \"" + voice + "\" --device \"CABLE Input\" --text64 " + base64,
+                Arguments = "--voice \"" + voice + "\" --device \"default\" --max-length 300 --text64 " + base64,
                 UseShellExecute = false,
                 CreateNoWindow = true,
                 WindowStyle = ProcessWindowStyle.Hidden
